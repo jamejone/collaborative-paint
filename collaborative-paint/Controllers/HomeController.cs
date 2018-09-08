6955313617b0fef5a0f0f770c6a839f88bd0b134
James Jones <james.russel.jones@gmail.com>
2018-09-08 19:09:57 -0400

    Set up the root web page

---
+++ collaborative-paint/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace collaborative_paint.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return View();
+        }
+    }
+}
