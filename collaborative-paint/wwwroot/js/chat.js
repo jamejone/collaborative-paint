1dfff0360334086db2032f0d5782cc4fbc72dea9
James Jones <james.russel.jones@gmail.com>
2018-09-09 17:55:18 -0400

    Improve touch compatibility.

--- collaborative-paint/wwwroot/js/chat.js
+++ collaborative-paint/wwwroot/js/chat.js
@@ -51,23 +51,40 @@ var x = "black",
 y = 2;
 
 function init() {
-canvas = document.getElementById('can');
-ctx = canvas.getContext("2d");
-w = canvas.width;
-h = canvas.height;
+    canvas = document.getElementById('can');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
 
-canvas.addEventListener("mousemove", function (e) {
-    findxy('move', e)
-}, false);
-canvas.addEventListener("mousedown", function (e) {
-    findxy('down', e)
-}, false);
-canvas.addEventListener("mouseup", function (e) {
-    findxy('up', e)
-}, false);
-canvas.addEventListener("mouseout", function (e) {
-    findxy('out', e)
-}, false);
+    canvas.addEventListener("mousemove", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+
+    canvas.addEventListener("touchstart", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+    canvas.addEventListener("touchend", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
+    canvas.addEventListener("touchmove", function (e) {
+        findxy(e)
+        e.preventDefault();
+    }, false);
 }
 
 function color(obj) {
@@ -122,12 +139,20 @@ if (m) {
 }
 }
 
-function findxy(res, e) {
-if (res == 'down') {
+function findxy(e) {
+if (e.type === 'touchstart' || e.type === 'mousedown') {
     prevX = currX;
     prevY = currY;
-    currX = e.clientX - canvas.offsetLeft;
-    currY = e.clientY - canvas.offsetTop;
+
+    if (e.type === 'mousedown') {
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+    }
+
+    if (e.type === 'touchstart') {
+        currX = e.touches[0].clientX - canvas.offsetLeft;
+        currY = e.touches[0].clientY - canvas.offsetTop;
+    }
 
     flag = true;
     dot_flag = true;
@@ -139,15 +164,23 @@ if (res == 'down') {
         dot_flag = false;
     }
 }
-if (res == 'up' || res == "out") {
+if (e.type === 'touchend' || e.type === 'mouseup' || e.type === 'mouseout') {
     flag = false;
 }
-if (res == 'move') {
+if (e.type === 'mousemove' || e.type === 'touchmove' ) {
     if (flag) {
         prevX = currX;
         prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
+
+        if (e.type === 'mousemove') {
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+        }
+    
+        if (e.type === 'touchmove') {
+            currX = e.touches[0].clientX - canvas.offsetLeft;
+            currY = e.touches[0].clientY - canvas.offsetTop;
+        }
         draw();
     }
 }
