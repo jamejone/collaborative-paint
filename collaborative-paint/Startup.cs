df8b13eae5e5f21fa1675ebde7106ff2394e10a1
James Jones <james.russel.jones@gmail.com>
2018-09-09 17:28:39 -0400

    Set up Azure SignalR and user secrets id

--- collaborative-paint/Startup.cs
+++ collaborative-paint/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using SignalRChat.Hubs;
 
 namespace collaborative_paint
@@ -28,7 +21,9 @@ namespace collaborative_paint
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddSignalR();
+            services
+                .AddSignalR()
+                .AddAzureSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +43,7 @@ namespace collaborative_paint
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseSignalR(route =>
+            app.UseAzureSignalR(route =>
             {
                 route.MapHub<StreamHub>("/streamHub");
                 route.MapHub<ChatHub>("/chatHub");
